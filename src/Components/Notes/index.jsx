956505f7de039c664b22e2d714f56e33b28fa887
AaronVickers <aaron_vickers@hotmail.co.uk>
2021-02-08 10:15:18 +0000

    Notes list to view, update and delete notes

--- src/Components/Notes/index.jsx
+++ src/Components/Notes/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 import { useNotes, NotesProvider } from "../../Contexts/Notes"
 
@@ -26,24 +26,57 @@ export const NewNote = () => {
   )
 }
 
-export const Note = () => {
+export const Note = (props) => {
   const { updateNote, deleteNote } = useNotes()
 
+  const note = props.note
+
+  const updateNoteClick = () => {
+    updateNote(note.id, "Updated content")
+  }
+
+  const deleteNoteClick = () => {
+    deleteNote(note.id)
+  }
+
   return (
-    <div className="note">
+    <li className="note">
+      <p className="note-content">{note.content}</p>
       
-    </div>
+      <button className="btn-update-note" onClick={updateNoteClick}>Update</button>
+      <button className="btn-delete-note" onClick={deleteNoteClick}>Delete</button>
+    </li>
   )
 }
 
 export const NotesList = () => {
   const { getNotes } = useNotes()
 
-  getNotes()
+  const [notes, setNotes] = useState([])
+  
+  useEffect(() => {
+    getNotes().then((notesObject) => {
+      let newNotes = []
+  
+      for (let id in notesObject) {
+        notesObject[id]["id"] = id
+  
+        newNotes.push(notesObject[id])
+      }
+  
+      setNotes(newNotes)
+    })
+  }, [getNotes])
 
   return (
     <div className="notes-list">
-
+      <ul>
+        {
+          notes.map((note) => {
+            return <Note note={note} key={note.id} />
+          })
+        }
+      </ul>
     </div>
   )
 }
